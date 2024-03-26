e986c386d2b790617311459fc25c8aec26d6c5b8
6842 Studio <122149669+fthr6842@users.noreply.github.com>
2024-03-26 09:19:05 +0800

    Delete 6_1.py

--- 6_1.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-s0, s1 = [(2*i-1)/i**2 for i in range(1, 101)], [2/(i-1) for i in range(2, 101)]
-plt.plot([i for i in range(1, 101)], s0, c="red")
-plt.plot([i for i in range(2, 101)], s1, c="black")
-plt.show()
