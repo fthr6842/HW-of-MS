499a595d048cc808a26619299bccbf57cac21bc4
6842 Studio <122149669+fthr6842@users.noreply.github.com>
2024-03-26 09:18:15 +0800

    Delete 12.2.py

--- 12.2.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-x = [0.01*i for i in range(0, 130)]
-y1 = [1 for i in range(0, 51)]
-y2 = [1/((2*x[i])**4) for i in range(51, 101)]
-y3 = [(1-(15/((2*x[i])**4))) for i in range(101, 130)]
-y = y1 + y2 + y3
-plt.plot(x, y)
-plt.show()
