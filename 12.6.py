0107880ce137df38ab11d17dcc8320f16f10bb33
6842 Studio <122149669+fthr6842@users.noreply.github.com>
2024-03-26 09:18:38 +0800

    Delete 12.6.py

--- 12.6.py
+++
@@ -1,8 +0,0 @@
-from statistics import mean, variance
-import yfinance as yf
-import scipy.stats
-data = yf.download("2330.tw", "2022-01-01", "2022-12-31")
-x = data["Close"].to_list()
-def t(x): return (mean(x) - 0.01)/variance(x)
-def pvalue(t, df): return scipy.stats.t.sf(abs(t), df)
-if __name__ == "__main__": print(0.5 - pvalue(t(x), len(x)-1))
