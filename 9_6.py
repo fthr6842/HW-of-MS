1130be010aea6ea6d9d86f80179f34523c4c3ec0
6842 Studio <122149669+fthr6842@users.noreply.github.com>
2024-03-26 09:19:40 +0800

    Delete 9_6.py

--- 9_6.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-from scipy.stats import norm
-def f0(x): return 1 - norm.cdf((75-x)/2)
-ydata = [f0(i) for i in range(50, 76)]
-plt.plot([i for i in range(50, 76)], ydata)
-plt.show()
